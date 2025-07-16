a91cf2c905b2ee25f6c4624a8058923bc8b41ef7
KzAshimo <good.ashimo@gmail.com>
2025-07-16 15:00:15 +0900

    store ①route インデント修正

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->prefix('v1')->group(function(){
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
 
-    Route::prefix('user')->group(function(){
+    Route::prefix('user')->group(function () {
         Route::post('store', [UserController::class], 'store');
     });
 });

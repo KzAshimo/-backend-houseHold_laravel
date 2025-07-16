17e61e3d60e3091a92f8dff20b8a0f8dc6dad967
KzAshimo <good.ashimo@gmail.com>
2025-07-16 14:59:46 +0900

    store ①route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->prefix('v1')->group(function(){
+
+    Route::prefix('user')->group(function(){
+        Route::post('store', [UserController::class], 'store');
+    });
+});

a0d3c1342ddb142791d71c8e6395040d42c10276
KzAshimo <good.ashimo@gmail.com>
2025-07-17 22:33:07 +0900

    show 1.controller 2.service 3.dto 4.resource 5.request 6.api 7.test

--- app/Http/Controllers/Api/IncomeController.php
+++ app/Http/Controllers/Api/IncomeController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Dto\Income\ShowDto;
 use App\Dto\Income\StoreDto;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Income\ShowRequest;
 use App\Http\Requests\Income\StoreRequest;
 use App\Http\Resources\Income\IndexResource;
+use App\Http\Resources\Income\ShowResource;
+use App\Http\Resources\User\ShowResource as UserShowResource;
 use App\Models\Income;
 use App\Services\Income\IndexService;
+use App\Services\Income\ShowService;
 use App\Services\Income\StoreService;
 use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -52,6 +58,16 @@ class IncomeController extends Controller
             Log::error($e);
             throw $e;
         }
+    }
+
+    // 収入詳細
+    public function show(ShowRequest $request, ShowService $service): ShowResource
+    {
+        $dto = new ShowDto((int)$request->route('income_id'));
+
+        $income = $service($dto);
+
+        return new ShowResource($income);
     }
 
 }

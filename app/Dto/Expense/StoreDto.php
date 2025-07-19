158b1e7ca7a75d2aeb740b7d767342d0ca9025f3
KzAshimo <good.ashimo@gmail.com>
2025-07-19 17:54:14 +0900

    store 1.controller 2.service 3.request 4.dto 5.test作成

--- app/Dto/Expense/StoreDto.php
+++ app/Dto/Expense/StoreDto.php
@@ -4,5 +4,11 @@ namespace App\Dto\Expense;
 
 class StoreDto
 {
-    //
+    public function __construct(
+        public readonly int $userId,
+        public readonly int $categoryId,
+        public readonly int $amount,
+        public readonly string $content,
+        public readonly string $memo,
+    ) {}
 }
--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Dto\Expense\StoreDto;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Expense\StoreRequest;
 use App\Http\Resources\Expense\IndexResource;
 use App\Models\Expense;
 use App\Services\Expense\IndexService;
+use App\Services\Expense\StoreService;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ExpenseController extends Controller
 {
@@ -22,8 +29,32 @@ class ExpenseController extends Controller
     }
 
     // --- 支出新規登録 ---
-    public function store()
+    public function store(StoreRequest $request, StoreService $service): JsonResponse
     {
-        return response()->json();
+        // リクエストでーら取得(dtoクラス使用)
+        $dto = new StoreDto(
+            userId: Auth::user()->id,
+            categoryId: $request->input('category_id'),
+            amount: $request->input('amount'),
+            content: $request->input('content'),
+            memo: $request->input('memo'),
+        );
+
+        DB::beginTransaction();
+        try {
+            // データ新規登録(serviceクラス使用)
+            $expenses = $service($dto);
+
+            DB::commit();
+
+            return response()->json([
+                'result' => true,
+                'expense' => $expenses,
+            ]);
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::error();
+            throw $e;
+        }
     }
 }

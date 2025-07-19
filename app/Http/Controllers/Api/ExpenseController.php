8bb1f945615bcdfecb09a5f7c824396d0f9481cf
KzAshimo <good.ashimo@gmail.com>
2025-07-19 18:33:53 +0900

    store 1.controller 2.model 3.test

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -6,12 +6,10 @@ use App\Dto\Expense\StoreDto;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Expense\StoreRequest;
 use App\Http\Resources\Expense\IndexResource;
-use App\Models\Expense;
 use App\Services\Expense\IndexService;
 use App\Services\Expense\StoreService;
 use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -31,7 +29,7 @@ class ExpenseController extends Controller
     // --- 支出新規登録 ---
     public function store(StoreRequest $request, StoreService $service): JsonResponse
     {
-        // リクエストでーら取得(dtoクラス使用)
+        // リクエストデータ取得(dtoクラスへ渡す)
         $dto = new StoreDto(
             userId: Auth::user()->id,
             categoryId: $request->input('category_id'),
@@ -43,17 +41,17 @@ class ExpenseController extends Controller
         DB::beginTransaction();
         try {
             // データ新規登録(serviceクラス使用)
-            $expenses = $service($dto);
+            $expense = $service($dto);
 
             DB::commit();
 
             return response()->json([
                 'result' => true,
-                'expense' => $expenses,
+                'expense' => $expense,
             ]);
         } catch (Exception $e) {
             DB::rollBack();
-            Log::error();
+            Log::error($e);
             throw $e;
         }
     }
--- tests/Feature/Expense/StoreTest.php
+++ tests/Feature/Expense/StoreTest.php
@@ -2,19 +2,48 @@
 
 namespace Tests\Feature\Expense;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class StoreTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+
+    // setup
+    #[\Override]
+    protected function setUp(): void
     {
-        $response = $this->get('/');
+        parent::setUp();
+        $this->seed();
+    }
+
+    public function test_storeExpense_成功(): void
+    {
+        // ログイン用にユーザデータ取得
+        $user = User::first();
+
+        // ログイン処理
+        $this->actingAs($user);
+
+        // リクエストデータ
+        $data = [
+            'category_id' => 2,
+            'amount' => 5000,
+            'content' => 'テスト費用',
+            'memo' => '受講料',
+        ];
+
+        $response = $this->postJson(('/api/v1/expense/store'), $data);
 
         $response->assertStatus(200);
+
+        // DB確認
+        $this->assertDatabaseHas('expenses', [
+            'category_id' => 2,
+            'amount' => 5000,
+            'content' => 'テスト費用',
+            'memo' => '受講料',
+        ]);
     }
 }

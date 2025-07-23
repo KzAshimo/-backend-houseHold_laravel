e9b4b7e00bb8f27d1cca066983dfeb7dd16bbd2f
KzAshimo <good.ashimo@gmail.com>
2025-07-24 02:11:21 +0900

    index 1.service 2.resource 3.controller

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Notification\IndexResource;
+use App\Services\Notification\IndexService;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class NotificationController extends Controller
 {
-    public function index()
+    public function index(IndexService $service): JsonResource
     {
-        return response()->json();
+        // お知らせ一覧データ取得：serviceクラス使用
+        $notification = $service();
+
+        // データを整形し返却：resourceクラス使用
+        return IndexResource::collection($notification);
     }
 }
--- app/Services/Notification/IndexService.php
+++ app/Services/Notification/IndexService.php
@@ -2,9 +2,12 @@
 
 namespace App\Services\Notification;
 
+use App\Models\Notification;
+
 class IndexService{
     public function __invoke()
     {
-        //
+        // データ一覧取得(対象データと関係ある[user]も取得)
+        return Notification::with(['user'])->paginate(10);
     }
 }

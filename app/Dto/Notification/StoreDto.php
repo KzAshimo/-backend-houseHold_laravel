b8c703ec81580031b1d356f0848491df8b616027
KzAshimo <good.ashimo@gmail.com>
2025-07-24 22:41:03 +0900

    store ファイル追加 1.request 2.service 3.dto

---
+++ app/Dto/Notification/StoreDto.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Dto\Notification;
+
+class StoreDto
+{
+    public function __construct(
+        //
+    ) {}
+}
---
+++ app/Services/Notification/StoreService.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Services\Notification;
+
+class StoreService{
+    public function __invoke()
+    {
+        //
+    }
+}

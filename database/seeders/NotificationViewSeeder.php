a24cb7167bd5b94c9216ff0e16241e1d424f52f1
KzAshimo <good.ashimo@gmail.com>
2025-07-26 21:41:51 +0900

    store データ追加 1.seed

---
+++ database/seeders/NotificationViewSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\NotificationView;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class NotificationViewSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $now = now();
+
+        NotificationView::insert([
+            [
+                'user_id' => 1,
+                'notification_id' => 1,
+                'viewed_at' => $now,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'user_id' => 1,
+                'notification_id' => 2,
+                'viewed_at' => $now,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]
+        ]);
+    }
+}

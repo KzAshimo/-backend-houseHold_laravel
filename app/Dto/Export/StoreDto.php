e8012293f83afe7319cdaecd90f337b6ac116532
KzAshimo <good.ashimo@gmail.com>
2025-07-30 20:42:44 +0900

    store ファイル作成 1.request 2.service 3.dto

---
+++ app/Dto/Export/StoreDto.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Dto\Export;
+
+class StoreDto
+{
+    public function __construct(
+        //
+    ) {}
+}
---
+++ app/Services/Export/StoreService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service\Export;
+
+class StoreService
+{
+    public function __invoke()
+    {
+        //
+    }
+}

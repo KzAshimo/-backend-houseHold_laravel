8ddd597a7dce9c5e297e4a76bbcbf76231a2653b
KzAshimo <good.ashimo@gmail.com>
2025-08-17 02:41:16 +0900

    seeder修正 service修正

--- app/Services/Expense/IndexService.php
+++ app/Services/Expense/IndexService.php
@@ -9,6 +9,6 @@ class IndexService
     public function __invoke()
     {
         // データ一覧取得(対象データと関係のある [user / category] )
-        return Expense::with(['user', 'category'])->orderBy('created_at', 'desc')->paginate(10);
+        return Expense::with(['user', 'category'])->orderBy('created_at', 'desc')->get();
     }
 }

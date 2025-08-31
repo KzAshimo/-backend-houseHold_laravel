5ba407ed931eaecb1b415cfec7e764b85a0c5c75
KzAshimo <good.ashimo@gmail.com>
2025-08-31 15:31:48 +0900

    デプロイ先変更 クラウドランへ

--- config/cors.php
+++ config/cors.php
@@ -1,8 +1,20 @@
 <?php
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
+
+    'allowed_origins_patterns' => [
+        '~^https://householdnextjs\.vercel\.app$~',
+
+        '~^https://householdnext-.*\.vercel\.app$~',
+    ],
+
+
     'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
     'supports_credentials' => true,
 ];

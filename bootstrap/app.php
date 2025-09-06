345c0cd71d640fbd2b0413015f1348798d7dbaf9
KzAshimo <good.ashimo@gmail.com>
2025-09-06 21:12:33 +0900

    app修正

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
 
         // apiミドルウェアグループの先頭にSanctumのミドルウェアを強制的に追加
         $middleware->api(prepend: [
